855cd95857f0ed80f5d7e837e3e17f3d8dba8035
ROBERTO LUCIANO LESTE MURTA FILHO PR074102 <pr074102@pbh.rmi>
2019-09-05 17:54:18 -0300

    menu mobile

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,7 @@
+<li class="{{ Request::is('campo*') ? 'active' : '' }}">
+    <a href="{!! route('campo.index') !!}"><i class="fa fa-edit"></i><span>Vistoria em CAMPO</span></a>
+</li>
+
 <li class="{{ Request::is('home*') ? 'active' : '' }}">
     <a href="{!! route('home') !!}"><i class="fa fa-edit"></i><span>Home</span></a>
 </li>
@@ -8,5 +12,6 @@
 <li class="{{ Request::is('vistorias*') ? 'active' : '' }}">
     <a href="{!! route('vistorias.index') !!}"><i class="fa fa-edit"></i><span>Vistorias</span></a>
 </li>
+
 
 
